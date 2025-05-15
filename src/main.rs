e47e196a8cfd74c9b4b696c9b7947ae086033b5f
heyeuuu <2829004293@qq.com>
2025-05-15 20:55:35 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::prelude::Peripherals;
+use esp_idf_svc::sys::link_patches;
+use log::info;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // It is necessary to call this function once. 
+    // Otherwise, some runtime patches implemented by esp-idf-sys might not link properly.
+    link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    // Acquire the peripherals (pins, ADCs, etc.)
+    let peripherals = Peripherals::take()?;
+
+    // Initialize GPIO4 as an output pin
+    let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+
+    // Log a message to indicate the program has started
+    info!("Hello, ESP32!");
+
+    // Infinite loop to toggle the LED
+    loop {
+        // Turn the LED on
+        led.set_high()?;
+        info!("LED turned on!");
+
+        // Delay for 1000 milliseconds (1 second)
+        FreeRtos::delay_ms(1000);
+
+        // Turn the LED off
+        led.set_low()?;
+        info!("LED turned off!");
+
+        // Delay for another 1000 milliseconds (1 second)
+        FreeRtos::delay_ms(1000);
+    }
+
+    // This part is unreachable in this example as the loop never terminates.
+    // However, for completeness and to handle potential future modifications:
+    // Ok(())
+}
